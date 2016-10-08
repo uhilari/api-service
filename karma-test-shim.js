011679585cf610d559d75f88d3c0d90d4c5eaabc
Uber Elvis Hilari Gutierrez <Uber Elvis Hilari Gutierrez>
2016-10-08 12:57:06 -0500

    terminado el ambiente de desarrollo

---
+++ karma-test-shim.js
@@ -0,0 +1,64 @@
+__karma__.loaded = function() {};
+
+var builtPath = '/base/src/';
+var builtTest = '/base/test/';
+
+function isJsFile(path) {
+  return path.slice(-3) == '.js';
+}
+
+function isSpecFile(path) {
+  return /\.spec\.(.*\.)?js$/.test(path);
+}
+
+function isBuiltFile(path) {
+  return isJsFile(path) && ((path.substr(0, builtPath.length) == builtPath) || (path.substr(0, builtTest.length) == builtTest));
+}
+
+var allSpecFiles = Object.keys(window.__karma__.files)
+  .filter(isSpecFile)
+  .filter(isBuiltFile);
+
+System.config({
+  baseURL: '/base',
+
+  map: {
+    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+    '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js'
+  }
+});
+
+System.import('systemjs.config.js')
+	.then(initTestBed)
+	.then(initTesting);
+
+function initTestBed(){
+  return Promise.all([
+    System.import('@angular/core/testing'),
+    System.import('@angular/platform-browser-dynamic/testing')
+  ])
+
+  .then(function (providers) {
+    var coreTesting    = providers[0];
+    var browserTesting = providers[1];
+
+    coreTesting.TestBed.initTestEnvironment(
+      browserTesting.BrowserDynamicTestingModule,
+      browserTesting.platformBrowserDynamicTesting());
+  })
+}
+
+function initTesting(){
+	return Promise.all(
+		allSpecFiles.map(function(moduleName) {
+			return System.import(moduleName);
+		})
+	)
+	.then(__karma__.start, __karma__.error);
+}
